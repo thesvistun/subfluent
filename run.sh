62a3310ce50bd2ded045cb1d2f6107eefc844b7b
thesvistun <a.svistun@proton.me>
2026-08-12 19:43:53 +0300

    [app] Move app's files to a separate location

--- run.sh
+++ run.sh
@@ -9,17 +9,17 @@ fail() {
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 ## DB file name. Used in Python app as well.
-export APP_DB_FILE='words.db'
+export APP_DB_FILE="${SCRIPT_DIR}/words.db"
 
 ## TXT file with a list of the basic words (one per line).
-export APP_BASIC_WORDS_FILE='1-1000.txt'
+export APP_BASIC_WORDS_FILE="${SCRIPT_DIR}/1-1000.txt"
 
 if [ ! -f "${APP_DB_FILE}" ]; then
   ## Fail if sqlite3 isn't installed.
   sqlite3 --version >/dev/null 2>&1 || fail 'sqlite3 not found, but required to run this application.'
   ## Creating default DB.
-  sqlite3 "${SCRIPT_DIR}/${APP_DB_FILE}" ''
+  sqlite3 "${APP_DB_FILE}" ''
 fi
 
 docker build -t my-python-app ${SCRIPT_DIR}/tools/docker/
-docker run -it --rm -p 8080:5000 -e APP_BASIC_WORDS_FILE -e APP_DB_FILE -v ${SCRIPT_DIR}:${SCRIPT_DIR}:z -w ${SCRIPT_DIR} --name app my-python-app $@
+docker run -it --rm -p 8080:5000 -e APP_BASIC_WORDS_FILE -e APP_DB_FILE -v ${SCRIPT_DIR}:${SCRIPT_DIR}:z -w ${SCRIPT_DIR}/app --name app my-python-app $@
