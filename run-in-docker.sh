8ccc46e63b70075d0dec036abf952300931b8152
thesvistun <a.svistun@proton.me>
2026-08-15 14:50:13 +0300

    Add application versioning

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -8,6 +8,8 @@ fail() {
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
+readonly APP_VERSION="$(git describe --tag)"
+
 readonly IMAGE_NAME='my-python-app'
 
 readonly CONTAINER_NAME='app'
@@ -52,8 +54,9 @@ docker volume inspect "${VOLUME_NAME}" >/dev/null 2>&1 || init_volume "${VOLUME_
 
 ## Building Docker image with the app inside.
 docker build \
+  --build-arg VERSION="${APP_VERSION}" \
   --build-arg APP_DIR="${CONTAINER_APP_DIR}" \
-  -t "${IMAGE_NAME}" \
+  -t "${IMAGE_NAME}:${APP_VERSION}" \
   -f "${SCRIPT_DIR}/tools/docker/Dockerfile" \
   "${SCRIPT_DIR}"
 
@@ -63,4 +66,4 @@ docker run -it --rm \
   -e DB_FILE="${CONTAINER_APP_DB_FILE}" \
   -v "${VOLUME_NAME}":"${CONTAINER_APP_DATA_DIR}" \
   --name "${CONTAINER_NAME}" \
-  "${IMAGE_NAME}" $@
+  "${IMAGE_NAME}:${APP_VERSION}" $@
--- run-in-kind.sh
+++ run-in-kind.sh
@@ -3,9 +3,11 @@ set -e
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
+readonly APP_VERSION="$(git describe --tags)"
+
 readonly DOCKER_REGISTRY='localhost:5001'
 
-readonly IMAGE_NAME="${DOCKER_REGISTRY}/subfluent:latest"
+readonly IMAGE_NAME="${DOCKER_REGISTRY}/subfluent"
 
 readonly RELEASE_NAME="stage"
 
@@ -17,12 +19,15 @@ readonly CONTAINER_APP_DATA_DIR="${CONTAINER_APP_DIR}/data"
 
 ## Building Docker image with the app inside.
 docker build \
+  --build-arg VERSION="${APP_VERSION}" \
   --build-arg APP_DIR="${CONTAINER_APP_DIR}" \
-  -t "${IMAGE_NAME}" \
+  -t "${IMAGE_NAME}:${APP_VERSION}" \
   -f "${SCRIPT_DIR}/tools/docker/Dockerfile" \
   "${SCRIPT_DIR}"
 
 ## Pusing Docker image to the Docker registry.
-docker push "${IMAGE_NAME}"
+docker push "${IMAGE_NAME}:${APP_VERSION}"
+
+yq ".appVersion = \"${APP_VERSION}\"" -i tools/helm/Chart.yaml
 
 helm install --set dataPath="${CONTAINER_APP_DATA_DIR}" "${RELEASE_NAME}" tools/helm
